c786da6c9fb7970c72b4509b3dec4ce942796e9d
Beyz61 <6trabzn1@gmail.com>
2024-11-07 16:12:47 +0100

    Added Screens

--- lib/bottom_navigation_bar_screen.dart
+++ lib/bottom_navigation_bar_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ps_16_timerapp/stopwatch_screen.dart';
+import 'package:ps_16_timerapp/timer_screen.dart';
 
 class BottomNavigationBarScreen extends StatefulWidget {    // statefull statt stateless
   const BottomNavigationBarScreen({
@@ -6,18 +8,32 @@ class BottomNavigationBarScreen extends StatefulWidget {    // statefull statt s
   });
 
   @override
-  State<BottomNavigationBarScreen> createState() => _BottomNavigationBarScreenState();
+  State<BottomNavigationBarScreen> createState() =>
+   _BottomNavigationBarScreenState();
 }
-
 class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
 
 int currentIndex = 0;
 late List <Widget> widgets;
-
+ @override
+ void initState() {
+    super.initState();
+    widgets = [
+      const StopwatchScreen(),
+      const TimerScreen(),
+    ];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
-      bottomNavigationBar: NavigationBar( destinations: const [
+      bottomNavigationBar: NavigationBar( 
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index){
+          currentIndex = index;
+          setState(() {
+          });
+        },
+        destinations: const [
         NavigationDestination(icon: Icon(Icons.alarm),label: "Stopwatch"),
         NavigationDestination(icon: Icon(Icons.timer),label: "Timer"),
       ],
