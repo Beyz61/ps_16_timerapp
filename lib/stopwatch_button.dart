fed29085f498959d2d92f5820fd2af5ef1346604
Beyz61 <6trabzn1@gmail.com>
2024-11-07 21:39:41 +0100

    added commit

--- lib/stopwatch_button.dart
+++ lib/stopwatch_button.dart
@@ -2,20 +2,26 @@ import 'package:flutter/material.dart';
 
 class StopwatchButton extends StatelessWidget {
   const StopwatchButton({
-    super.key, required this.onTap, required this.buttonName
+    super.key, required this.onTap, required this.buttonName, required this.color,
+    this.textColor = const Color.fromARGB(255, 3, 20, 35),
   });
 final void Function()? onTap;  
+final Color textColor;
+final Color color;
 final String buttonName;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
+      
       child: Container(
         height: 40,
         width: 96,
-        decoration: BoxDecoration(
+        decoration: BoxDecoration( 
+          color: color,
         border: Border.all(color:const Color.fromARGB(255, 7, 46, 77), 
+        
         ),
         borderRadius: BorderRadius.circular(32),
         ),
--- lib/timer_screen.dart
+++ lib/timer_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:ps_16_timerapp/stopwatch_button.dart';
 
 class TimerScreen extends StatelessWidget {
   const TimerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 147, 181, 209),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 7, 46, 77),
+        title: const Text("Timer",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 30, 
+          fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("15 s",
+          style: TextStyle(
+            fontSize: 88,
+            fontWeight: FontWeight.bold,
+            color:  Color.fromARGB(255, 7, 46, 77),
+          ),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            StopwatchButton(onTap: null, buttonName: "Start", color: Colors.grey),
+            StopwatchButton(onTap: null, buttonName: "Stop", color: Colors.red),
+          ],),
+        ],
+      ),
+    );
   }
 }
