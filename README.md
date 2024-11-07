4165bf801fab3d5f77341c5dd0e7dbb2c7899ae0
Beyz61 <6trabzn1@gmail.com>
2024-11-07 11:18:34 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ps_16_timerapp
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ps_16_timerapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ps_16_timerapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
